9317075d7ec4da7f46a1e70a32417f5270346525
Carlos <carlos-84@live.com>
2022-01-15 11:53:22 -0300

    c# com json usando a api newtonsoft

---
+++ Principal.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1.LendoDadosJson
+{
+    internal class Principal
+    {
+        static void Main(string[] args)
+        {
+            var json = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"\LendoDadosJson\dados.json");
+
+            var user = JsonConvert.DeserializeObject<List<Usuarios>>(json);
+
+            Usuarios usuarios = new Usuarios("henrique", 4567);
+            
+            user.Add(usuarios);
+
+            var json_serializado = JsonConvert.SerializeObject(user);
+
+            Console.WriteLine(json_serializado);
+
+            
+            
+                
+            
+            
+
+        }
+    }
+}
---
+++ Usuarios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.LendoDadosJson
+{
+    internal class Usuarios
+    {
+        public string login { get; set; }
+        public int senha { get; set; }
+
+        public Usuarios(string login, int senha)
+        {
+            this.login = login;
+            this.senha = senha;
+        }
+    }
+}
